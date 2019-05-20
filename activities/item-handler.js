fbdc68d0edfb37c655c2cef2c5aeebce06abbcdf
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-05-20 16:08:08 +0200

    Initial commit.

---
+++ activities/item-handler.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = async (activity) => {
+  try {
+    if (!activity.Context.connector.custom1 || (activity.Context.connector.custom1 != activity.Request.Headers["x-apikey"])) {
+      activity.Response.ErrorCode = 403;
+      activity.Response.Data = {
+        ErrorText: "invalid X-APIKEY"
+      };
+      return;
+    }
+
+    var request = activity.Request.Data;
+    var entity = {};
+    var collections = [];
+
+    if (request._type) {
+      let date = new Date(request.date).toJSON();
+      entity = {
+        _type: request._type,
+        id: "" + request.id,
+        title: request.title,
+        description: request.description,
+        date: date,
+        link: request.link
+      };
+    }
+    activity.Response.Data = { entity: entity, collections: collections };
+  } catch (error) {
+    $.handleError(activity, error);
+  }
+};
