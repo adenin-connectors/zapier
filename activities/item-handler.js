d71bb7c7930d9e1dbfe23b7cb22945d3c81f121c
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-05-30 11:54:07 +0200

    Added collections to item-handler.js.

--- activities/item-handler.js
+++ activities/item-handler.js
@@ -9,12 +9,11 @@ module.exports = async (activity) => {
       return;
     }
 
-    if(activity.Request.Data._type == "validate_key") return;
+    if (activity.Request.Data._type == "validate_key") return;
 
-    var request = activity.Request.Data;
-    var entity = {};
-    var collections = [];
-
+    const request = activity.Request.Data;
+    let entity = {};
+    let collections = [];
     if (request._type) {
       let date = new Date(request.date).toJSON();
       entity = {
@@ -25,9 +24,29 @@ module.exports = async (activity) => {
         date: date,
         link: request.link
       };
+
+      if (request.openValue == true) {
+        if (request.assignedTo) {
+          // case 1: A collection “new” is returned with users and roles
+          collections.push({ name: "new", users: request.assignedTo, roles: request.roles, date: date });
+          // case 2: When open == true we return collection “open”, with users and roles
+          collections.push({ name: "open", users: request.assignedTo, roles: request.roles, date: date });
+          // case 3: When AssignedTo is not empty and open we return a collection “my”, with only users: AssignedTo  (no roles)
+          collections.push({ name: "my", users: request.assignedTo, roles: [], date: date });
+          if (request.dueDate) {
+            let date = new Date(request.dueDate).toISOString();
+            // case 4: When DueDate is provided and open we return a collection “due”, with users and roles; date = DueDate
+            collections.push({ name: "due", users: request.assignedTo, roles: request.roles, date: date });
+            // case 5: When DueDate is provided and open we return a collection “my-due”, with only users: AssignedTo  (no roles), date = DueDate
+            collections.push({ name: "my-due", users: request.assignedTo, roles: [], date: date });
+          }
+        }
+      }
     }
+
     activity.Response.Data = { entity: entity, collections: collections };
   } catch (error) {
     $.handleError(activity, error);
   }
+
 };
