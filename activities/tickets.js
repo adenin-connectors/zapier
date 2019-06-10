ebfd56b38ba875d70a724e574cf12fa9efb9ae70
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-06-10 12:42:17 +0200

    Added 'ticket' activities.

--- activities/tickets.js
+++ activities/tickets.js
@@ -7,25 +7,25 @@ module.exports = async function (activity) {
 
     const pagination = $.pagination(activity);
     const dateRange = $.dateRange(activity);
-    const response = await api(`/issue/my?page=${pagination.page}&pageSize=${pagination.pageSize}` +
+    const response = await api(`/ticket/all?page=${pagination.page}&pageSize=${pagination.pageSize}` +
       `&startDate=${dateRange.startDate}&endDate=${dateRange.endDate}`);
 
     if ($.isErrorResponse(activity, response)) return;
 
     activity.Response.Data.items = response.body.Data.items;
     let value = response.body.Data.count;
-    activity.Response.Data.title = T(activity, 'My Issues');
+    activity.Response.Data.title = T(activity, 'All Tickets');
     activity.Response.Data.link = "";
-    activity.Response.Data.linkLabel = T(activity, 'All Issues');
+    activity.Response.Data.linkLabel = T(activity, 'All Tickets');
     activity.Response.Data.actionable = value > 0;
 
     if (value > 0) {
       activity.Response.Data.value = value;
       activity.Response.Data.color = 'blue';
-      activity.Response.Data.description = value > 1 ? T(activity, "You have {0} assigned issues.", value)
-        : T(activity, "You have 1 assigned issue.");
+      activity.Response.Data.description = value > 1 ? T(activity, "There are {0} tickets.", value)
+        : T(activity, "There is 1 ticket.");
     } else {
-      activity.Response.Data.description = T(activity, `You have no issues assigned.`);
+      activity.Response.Data.description = T(activity, 'There are no tickets.');
     }
   } catch (error) {
     $.handleError(activity, error);
