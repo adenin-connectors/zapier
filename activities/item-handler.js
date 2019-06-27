2f40091bb65cd6304ef66f6e889a8f17990c7aab
olliejm <olliejm@protonmail.com>
2019-06-27 16:46:30 +0100

    debug error in azure

--- activities/item-handler.js
+++ activities/item-handler.js
@@ -2,6 +2,7 @@
 module.exports = async (activity) => {
   try {
     if (!activity.Context.connector.custom1 || (activity.Context.connector.custom1 != activity.Request.Headers["X-Apikey"])) {
+      logger.error('403', activity.Context.connector, activity.Request.Headers);
       activity.Response.ErrorCode = 403;
       activity.Response.Data = {
         ErrorText: "invalid X-APIKEY"
